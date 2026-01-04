6152a6826d6e1b5c0e948e778c847429bb178e37
TranThanhHai104 <22130070@st.hcmuaf.edu.vn>
2026-01-04 20:33:31 +0700

    sua 1 chut css de co the hien from dang nhap

--- assets/js/templates.js
+++ assets/js/templates.js
@@ -1,10 +1,16 @@
-function includeHTML(elementId, filePath) {
+function includeHTML(elementId, filePath, callback) {
     fetch(filePath)
         .then(res => res.text())
         .then(data => {
             document.getElementById(elementId).innerHTML = data;
+            if (callback) callback();
         });
 }
 
-includeHTML('header-id', 'templates/header.html');
+includeHTML('header-id', 'templates/header.html', function () {
+    document
+        .getElementById("btn-login")
+        .addEventListener("click", openAuth);
+});
+
 includeHTML('footer-id', 'templates/footer.html');
