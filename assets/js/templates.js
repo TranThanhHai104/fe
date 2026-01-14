850b3b06552ae04ff3ce7331de004a59b60ad128
TranThanhHai104 <22130070@st.hcmuaf.edu.vn>
2026-01-14 23:32:10 +0700

    them che do chuyen theme cho trang tin tuc

--- assets/js/templates.js
+++ assets/js/templates.js
@@ -6,5 +6,38 @@ function includeHTML(elementId, filePath, callback) {
             if (callback) callback();
         });
 }
+function includeHTML(elementId, filePath, callback) {
+    fetch(filePath)
+        .then(res => res.text())
+        .then(data => {
+            document.getElementById(elementId).innerHTML = data;
+            if (callback) callback();
+        });
+}
 
+document.addEventListener("DOMContentLoaded", function () {
+    if (localStorage.getItem("theme") === "dark") {
+        document.body.classList.add("dark-mode");
+    }
+    //load header truoc
+    includeHTML("header-id", "templates/header.html", function () {
+        let nut = document.getElementById("theme-toggle");
+        let text = document.getElementById("theme-icon");
 
+        if (!nut || !text) return;
+
+        text.innerText = document.body.classList.contains("dark-mode") ? "Sáng" : "Tối";
+
+        nut.addEventListener("click", function () {
+            document.body.classList.toggle("dark-mode");
+
+            if (document.body.classList.contains("dark-mode")) {
+                localStorage.setItem("theme", "dark");
+                text.innerText = "Sáng";
+            } else {
+                localStorage.setItem("theme", "light");
+                text.innerText = "Tối";
+            }
+        });
+    });
+});
